fde9df679ba8e337e7a691445a0a662837eba54e
chriss1525 <okothchris15@gmail.com>
2023-10-01 00:55:52 +0300

    add header rules

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const admin = require('firebase-admin')
 const port = process.env.PORT || 3000;
 
 require('./utils/db.js');
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    // ... other CORS headers ...
+    next();
+});
 // import routes
 app.use('/extension', require('./routes/index.js'));
 
